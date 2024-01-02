b4d7bc3a02f460399d97638a079cdad1a7458165
novventa <108192878+novventa@users.noreply.github.com>
2024-01-02 17:08:32 +0900

    [Bronze III] Title: 통학의 신, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int x = -1000;
+        List<Integer> ans = new ArrayList<>();
+
+        while(x<1001){
+            if(((x*x+2*A*x+B)==0) && !ans.contains(x))
+                ans.add(x);
+            x++;
+        }
+
+        for(int num : ans){
+            System.out.print(num);
+            System.out.print(" ");
+        }
+    }
+}
