bb4ade525d64e3d50c400a3bfb76b62ad9b41c0b
novventa <108192878+novventa@users.noreply.github.com>
2024-01-02 17:05:43 +0900

    [Silver II] Title: 창고 다각형, Time: 148 ms, Memory: 14544 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[1001];
+        int start = Integer.MAX_VALUE;
+        int end = 0;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+            arr[l] = h;
+            start = Math.min(l, start);
+            end = Math.max(l, end);
+        }
+
+        Stack<Integer> height = new Stack<>();
+
+        int temp = arr[start];
+        for (int i = start + 1; i <= end; i++) {
+            if(arr[i] < temp)  {
+                height.push(i);
+            }
+            else {
+                while (!height.isEmpty()) {
+                    int x = height.pop();
+                    arr[x] = temp;
+                }
+                temp = arr[i];
+            }
+        }
+        height.clear();
+
+        temp=arr[end];
+        for(int i = end - 1; i >= start; i--){
+            if(arr[i] < temp) height.push(i);
+            else {
+                while (!height.isEmpty()) {
+                    int x = height.pop();
+                    arr[x]=temp;
+                }
+                temp=arr[i];
+            }
+        }
+
+        int result = 0;
+        for (int i = start; i <= end; i++) {
+            result += arr[i];
+        }
+
+        sb.append(result).append("\n");
+        System.out.print(sb);
+    }
+}
