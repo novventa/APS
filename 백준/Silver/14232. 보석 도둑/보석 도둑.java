d454a5b696961db6b85cb47aac0c4820d8a3ae79
novventa <108192878+novventa@users.noreply.github.com>
2024-01-03 17:17:23 +0900

    [Silver I] Title: 보석 도둑, Time: 256 ms, Memory: 18372 KB -BaekjoonHub

---
+++ 백준/Silver/14232. 보석 도둑/보석 도둑.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long k = sc.nextLong();
+        ArrayList<Long> list = new ArrayList<>();
+
+        for (long i = 2; i <= Math.ceil(Math.sqrt(k)); i++) {
+            while (k % i == 0) {
+                list.add(i);
+                k /= i;
+            }
+        }
+
+        if (k != 1) {
+            list.add(k);
+        }
+
+        System.out.println(list.size());
+        for (long factor : list) {
+            System.out.print(factor + " ");
+        }
+    }
+}
