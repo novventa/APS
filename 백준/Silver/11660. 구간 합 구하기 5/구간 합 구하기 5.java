bc57635a87b8c9c941e01d7365a115491b5af225
novventa <108192878+novventa@users.noreply.github.com>
2024-01-11 17:38:12 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1760 ms, Memory: 123964 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] table = new int[N + 1][N + 1];
+
+        // 표 입력
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                table[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 각 지점까지의 누적 합 계산
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                table[i][j] += table[i - 1][j] + table[i][j - 1] - table[i - 1][j - 1];
+            }
+        }
+
+        // 각 쿼리에 대해 합 출력
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int result = table[x2][y2] - table[x2][y1 - 1] - table[x1 - 1][y2] + table[x1 - 1][y1 - 1];
+            System.out.println(result);
+        }
+    }
+}
