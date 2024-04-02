2e17f89c5d19835b792ca6baeafb76a5fd150a26
novventa <108192878+novventa@users.noreply.github.com>
2024-04-02 16:02:40 +0900

    [Gold IV] Title: 트리의 지름, Time: 224 ms, Memory: 23416 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<Node>[] tree;
+    static boolean[] visited;
+    static int maxDistance;
+    static int farthestNode;
+
+    static class Node {
+        int child;
+        int weight;
+
+        Node(int child, int weight) {
+            this.child = child;
+            this.weight = weight;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        tree = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int parent = Integer.parseInt(st.nextToken());
+            int child = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            tree[parent].add(new Node(child, weight));
+            tree[child].add(new Node(parent, weight));
+        }
+
+        maxDistance = 0;
+        farthestNode = 0;
+        dfs(1, 0);
+
+        visited = new boolean[n + 1];
+        maxDistance = 0;
+        dfs(farthestNode, 0);
+
+        System.out.println(maxDistance);
+    }
+
+    static void dfs(int node, int distance) {
+        visited[node] = true;
+
+        if (distance > maxDistance) {
+            maxDistance = distance;
+            farthestNode = node;
+        }
+
+        for (Node child : tree[node]) {
+            if (!visited[child.child]) {
+                dfs(child.child, distance + child.weight);
+            }
+        }
+    }
+}
