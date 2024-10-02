fff575569d572878dc6ecdec9e6902d51893d0bd
novventa <108192878+novventa@users.noreply.github.com>
2024-10-02 18:31:27 +0900

    [Gold V] Title: 도넛 행성, Time: 560 ms, Memory: 116876 KB -BaekjoonHub

---
+++ 백준/Gold/27211. 도넛 행성/도넛 행성.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n,m;
+    static int[][] map;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 0 && !visited[i][j]) {
+                    dfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
+
+    private static void dfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        visited[x][y] = true;
+        queue.offer(new int[]{x, y});
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        while (!queue.isEmpty()) {
+            int[] pos = queue.poll();
+            int cx = pos[0];
+            int cy = pos[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = (cx + dx[i] + n) % n;
+                int ny = (cy + dy[i] + m) % m;
+                
+                if (map[nx][ny] == 0 && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    queue.offer(new int[]{nx, ny});
+                }
+            }
+        }
+    }
+}
